0cb84fada2e7514dcef63e1f2fec771c7b6c15dd
liamdoan <lamdoan9898@gmail.com>
2024-11-07 02:33:40 +0200

    fix border

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import './SideNav.css'
+import { useTheme } from '../context/ThemeContext'
 import GithubLogo from "../img/logo/github-icon.png"
 import LinkedinLogo from "../img/logo/linkedin-icon.png"
 
-
 const SideNav = () => {
+    const {theme} = useTheme();
+
     return (
         <div className="sidenav">
             <div className="sidenav-wrap">
                 <div className="sidenav-icons">
                     <div className="sidenav-github">
                         <a href="//github.com/liamdoan" target="_blank">
-                            <img className="sidenav-github-logo" src={GithubLogo} alt="github" />
+                            <img className={`sidenav-github-logo ${theme}`} src={GithubLogo} alt="github" />
                         </a>
                     </div>
                     <div className="sidenav-linkedin">
                         <a href="//www.linkedin.com/in/lamdoan98" target="_blank">
-                            <img className="sidenav-linkedin-logo" src={LinkedinLogo} alt="linkedin" />
+                            <img className={`sidenav-linkedin-logo ${theme}`} src={LinkedinLogo} alt="linkedin" />
                         </a>
                     </div>
                 </div>
